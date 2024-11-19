9c4ad48945cd58b5ca86a419fb99138812ea503f
Davidcmd402 <david.vieira23a@gmail.com>
2024-11-19 16:35:59 -0300

    Spring Boot project

---
+++ src/test/java/com/davidvieira/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidvieira.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

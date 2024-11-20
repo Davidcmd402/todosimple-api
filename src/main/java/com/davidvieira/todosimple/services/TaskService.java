fc8a50c606bef635f23a2747ca685c62975ada94
Davidcmd402 <david.vieira23a@gmail.com>
2024-11-20 15:56:48 -0300

    Create Task service

---
+++ src/main/java/com/davidvieira/todosimple/services/TaskService.java
@@ -0,0 +1,54 @@
+package com.davidvieira.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.davidvieira.todosimple.models.Task;
+import com.davidvieira.todosimple.models.User;
+import com.davidvieira.todosimple.repositories.TaskRepository;
+
+@Service
+public class TaskService {
+    
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Transactional
+    public Task findById(long id) {
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(()-> new RuntimeException(
+            "Task not found. id: " + ", Type: " + Task.class.getName()
+        ));
+    }
+
+    @Transactional
+    public Task create(Task obj) {
+        User user = this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj) {
+        Task newobj = findById(obj.getId());
+        newobj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newobj);
+    }
+    
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.taskRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Not can delete.");
+        }
+    }
+}

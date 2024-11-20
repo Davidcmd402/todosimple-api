f5cc7661db46416da80aa0c5ca2fb89621ece3d0
Davidcmd402 <david.vieira23a@gmail.com>
2024-11-20 14:37:11 -0300

    Create Task Model

--- src/main/java/com/davidvieira/todosimple/models/User.java
+++ src/main/java/com/davidvieira/todosimple/models/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -40,7 +41,9 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class},  min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
 
     public User() {
@@ -77,4 +80,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
 }

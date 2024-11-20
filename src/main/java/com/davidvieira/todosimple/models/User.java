d3271c72a06f0fb80bec14823fffc65329ad8f6a
Davidcmd402 <david.vieira23a@gmail.com>
2024-11-20 12:34:37 -0300

    Create User Model

---
+++ src/main/java/com/davidvieira/todosimple/models/User.java
@@ -0,0 +1,80 @@
+package com.davidvieira.todosimple.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+@Entity
+@Table(name= User.TABLE_NAME)
+public class User {
+    public interface CreateUser {}
+    public interface UpdateUser {}
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+
+    @Column(name = "userName", length = 100, nullable = false, unique = true)
+    @NotBlank(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 2, max = 100)
+    private String userName;
+
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", length = 100, nullable = false)
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class},  min = 8, max = 60)
+    private String password;
+
+    //private List<Task> tasks = new ArrayList<Task>();
+
+
+    public User() {
+    }
+
+
+    public User(Long id, String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

7912f849ca3c00a02a5828e59caea6c3b545a0ae
Davidcmd402 <david.vieira23a@gmail.com>
2024-11-25 16:18:24 -0300

    refactor webconfigure

--- src/main/java/com/davidvieira/todosimple/configs/WebConfig.java
+++ src/main/java/com/davidvieira/todosimple/configs/WebConfig.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     
     public void addCorsMapping(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**") // Aplica CORS a todos os endpoints
+            .allowedOrigins("http://localhost:8080", "http://127.0.0.1:5500") // Permite origens específicas
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+            .allowedHeaders("*"); // Permite todos os cabeçalhos
     }
 }

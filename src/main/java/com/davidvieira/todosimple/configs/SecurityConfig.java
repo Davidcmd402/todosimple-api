53a23c7c2f5c9f0e70687f286739fe2936f869e5
Davidcmd402 <david.vieira23a@gmail.com>
2024-12-28 08:46:46 -0300

    add password encoder and create profileenum

--- src/main/java/com/davidvieira/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/davidvieira/todosimple/configs/SecurityConfig.java
@@ -2,19 +2,14 @@ package com.davidvieira.todosimple.configs;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
--- src/main/java/com/davidvieira/todosimple/services/UserService.java
+++ src/main/java/com/davidvieira/todosimple/services/UserService.java
@@ -1,18 +1,25 @@
 package com.davidvieira.todosimple.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.davidvieira.todosimple.models.User;
+import com.davidvieira.todosimple.models.enums.ProfileEnum;
 import com.davidvieira.todosimple.repositories.UserRepository;
 import com.davidvieira.todosimple.services.exceptions.DataBindingViolationException;
 import com.davidvieira.todosimple.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     
     @Autowired
     private UserRepository userRepository;
@@ -27,6 +34,8 @@ public class UserService {
     @Transactional
     public User create(User obj) {
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -35,6 +44,7 @@ public class UserService {
     public User update(User obj) {
         User newobj = findById(obj.getId());
         newobj.setPassword(obj.getPassword());
+        newobj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newobj);
     }
 

a23bf9f9d427c164b67fce17d15c54f14b749893
Davidcmd402 <david.vieira23a@gmail.com>
2024-12-28 09:02:45 -0300

    create UserSpringSecurity

---
+++ src/main/java/com/davidvieira/todosimple/security/UserSpringSecurity.java
@@ -0,0 +1,53 @@
+package com.davidvieira.todosimple.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.davidvieira.todosimple.models.enums.ProfileEnum;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+
+    public UserSpringSecurity(Long id, String username, String password, Set<ProfileEnum> profileEnums) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = profileEnums.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    public boolean isCrednetialsNonExpired() {
+        return true;
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean hasRole(ProfileEnum profileEnum) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
+    }
+    
+}

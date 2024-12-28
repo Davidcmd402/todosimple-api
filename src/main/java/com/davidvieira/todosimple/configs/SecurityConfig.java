af30d5a69e42e5cf05fc76bd46a7a6aed01851b6
Davidcmd402 <david.vieira23a@gmail.com>
2024-12-28 10:02:59 -0300

    create userDetailsServiceImpl and JWTUtil

--- src/main/java/com/davidvieira/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/davidvieira/todosimple/configs/SecurityConfig.java
@@ -2,21 +2,35 @@ package com.davidvieira.todosimple.configs;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.davidvieira.todosimple.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
     
     private static final String[] PUBLIC_MATCHERS = {
         "/"
@@ -29,6 +43,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(this.userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+
+        this.authenticationManager = authenticationManagerBuilder.build();
+
         return http
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
